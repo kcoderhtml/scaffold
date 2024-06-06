a6c8f9611c1353d4ede47a1c3a52726388ddb96b
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-06 17:08:52 -0400

    refactor: remove unused imports and dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,3 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-	DarkTheme,
-	DefaultTheme,
-	ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
