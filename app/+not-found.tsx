a4adebeb81860841e44deaa5c277dded264ed41c
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-06 18:11:59 -0400

    refactor: Update NotFoundScreen component with new UI design

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -3,10 +3,29 @@ import { Link, Stack } from "expo-router";
 import React from "react";
 import { Text, View, Pressable } from "react-native";
 
+import { useColorScheme } from "react-native";
+import colors from "tailwindcss/colors";
+
 export default function NotFoundScreen() {
+	const colorScheme = useColorScheme();
+
+	const backgroundColor =
+		colorScheme === "dark" ? colors.gray[800] : colors.gray[100];
+	const headerTextColor =
+		colorScheme === "dark" ? colors.slate[200] : colors.slate[900];
+
 	return (
 		<View className="flex-1 items-center justify-center bg-slate-50 dark:bg-slate-700">
-			<Stack.Screen options={{ title: "Oops!" }} />
+			<Stack.Screen
+				options={{
+					title: "Oops!",
+					headerStyle: {
+						backgroundColor: backgroundColor,
+					},
+					headerTitleStyle: { color: headerTextColor },
+					headerBackTitle: "Back",
+				}}
+			/>
 
 			<Text className="text-2xl font-bold text-slate-900 dark:text-slate-50">
 				This screen doesn't exist.
