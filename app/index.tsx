cf0a0094b23c21be7cbc85259fd8c3df08a1bfaa
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-06 16:02:44 -0400

    feat: add tailwind css

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 items-center justify-center bg-slate-50 dark:bg-slate-500"
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
