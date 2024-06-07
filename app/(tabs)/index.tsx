257736b89b9c1bea9dcf7fbc06e74074543f8818
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-06 21:01:35 -0400

    feat: add card component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,63 @@
 import { Stack } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
+
+import { useColorScheme } from "react-native";
+import colors from "tailwindcss/colors";
+
+import Card from "../../components/card";
 
 export default function Home() {
-	return (
-		<View className="flex-1 items-center justify-center bg-slate-50 dark:bg-slate-700">
-			<Stack.Screen options={{ title: "Home" }} />
+	const colorScheme = useColorScheme();
 
-			<Text className="text-2xl font-bold text-slate-900 dark:text-slate-50">
-				Welcome to Scaffold 🚀
-			</Text>
+	const backgroundColor =
+		colorScheme === "dark" ? colors.gray[800] : colors.gray[100];
+
+	return (
+		<View className="bg-slate-50 dark:bg-slate-700">
+			<ScrollView
+				contentContainerStyle={{ flexGrow: 1 }}
+				overScrollMode="never"
+			>
+				<View className="flex-1 items-center">
+					<Stack.Screen options={{ title: "Home" }} />
+
+					<Text className="text-2xl font-bold mt-12 text-slate-900 dark:text-slate-50">
+						Welcome to Scaffold 🚀
+					</Text>
+
+					<Text className="text-slate-900 dark:text-slate-50 p-4">
+						Your mind is a garden, your thoughts are the seeds; you can grow
+						flowers, or you can grow weeds.
+					</Text>
+
+					<View className="flex flex-wrap">
+						<Card
+							title="test card"
+							description="a description that is rather small"
+						></Card>
+						<Card
+							title="test card with image"
+							description="a description that is rather small"
+							image="https://source.unsplash.com/random/800x600"
+						></Card>
+						<Card
+							title="test card with no image"
+							description="a bountifully long description that excedes the length of the previous cards"
+						></Card>
+						<Card
+							title="test card with image and description"
+							description="a description that is rather small"
+							image="https://source.unsplash.com/random/800x600"
+						></Card>
+
+						<Card
+							title="another test card"
+							description="a description; kinda long"
+						></Card>
+					</View>
+				</View>
+			</ScrollView>
 		</View>
 	);
 }
---
+++ components/card.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text, View, Image } from "react-native";
+
+interface CardProps {
+	image?: string;
+	title: string;
+	description: string;
+}
+
+const Card: React.FC<CardProps> = ({ image, title, description }) => {
+	return (
+		<View className="bg-slate-300 dark:bg-slate-600 rounded-lg shadow-md m-1">
+			{image && (
+				<Image
+					source={{ uri: image }}
+					className="w-56 h-56 object-cover rounded-t"
+				/>
+			)}
+			<View className="w-56">
+				<Text className="text-xl font-bold text-slate-900 dark:text-slate-50 p-2 text-center">
+					{title}
+				</Text>
+				<Text className="text-slate-900 dark:text-slate-50 pl-2 pr-2 pb-2 text-center">
+					{description}
+				</Text>
+			</View>
+		</View>
+	);
+};
+
+export default Card;
